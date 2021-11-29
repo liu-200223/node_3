909c4eb838849be01b17e79449ad343cb3fd55c4
liu-200223 <91165974+liu-200223@users.noreply.github.com>
2021-11-29 11:53:01 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const { urlencoded } = require('express')
+
+dotenv.config()
+const host = process.env.HOST
+const port = process.env.port
+
+const app = express()
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname + '/pubic'))
+app.post('/auth',(req,res) => {
+    const login_name = req.body.login_name
+    const password = req.body.password
+
+    let message = 'ログインできません'
+    if (login_name == process.env.LOGIN_NAME&& password
+        == process.env.PASSWORD){message ='ログインしました'
+        }
+
+res.send(message)
+    })
+// app.get(URL,)
+app.get('/',(req,res) => {
+    res.send('Hello YSE!!!!!')
+})
+
+app.get('/profile',(req,res) => {
+    res.send('This is Profile Page!!!')
+})
+
+app.listen(port,host, () => {
+    console.log('http://' + host + ':' + port)
+})
